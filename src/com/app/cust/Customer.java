3d3db68bcd2c734fa2b558675c8d9f0dce4f9748
vimleshverma <er.vimlesh@gmail.com>
2017-07-09 00:00:38 +0530

    Update Customer.java

--- src/com/app/cust/Customer.java
+++ src/com/app/cust/Customer.java
@@ -3,6 +3,8 @@ package com.app.cust;
 public class Customer {
 	public static void main(String[] args) {
 		System.out.println("Custome Module");
+		System.out.println("Custome Module1");
+		System.out.println("Custome Module2");
 	}
 
 }
